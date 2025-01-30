88cb108f3d811744fd8d598f5f08c2399e67561a
vvywasejected <vvy6816@gmail.com>
2025-01-30 23:45:31 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,8 +3,8 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
     Name = "Ordis Hub",
     Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-    LoadingTitle = "Rayfield Interface Suite",
-    LoadingSubtitle = "by Sirius",
+    LoadingTitle = "Ordis Hub",
+    LoadingSubtitle = "by OrdisHQ",
     Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
  
     DisableRayfieldPrompts = false,
