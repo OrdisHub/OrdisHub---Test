359fe243fc7d0512e40dfe82fe04486406a17fb4
vvywasejected <vvy6816@gmail.com>
2025-01-30 23:41:52 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "Rayfield Example Window",
+    Name = "Ordis Hub",
     Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
     LoadingTitle = "Rayfield Interface Suite",
     LoadingSubtitle = "by Sirius",
@@ -33,3 +33,5 @@ local Window = Rayfield:CreateWindow({
        Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
  })
+
+ local MainTab = Window:CreateTab("Main", 4483362458) -- Title, Image
