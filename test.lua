690b8dbc7aa945d257ebe8753d931c9e966c6e36
vvywasejected <vvy6816@gmail.com>
2025-01-30 23:32:45 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,16 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Please Work FFS", "Sentinel")
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/NICKISBAD/Nick-s-Modded-KAVO-Lib/main/Nick'sModdedKavoLib.lua"))()
 
-local Tab = Window:NewTab("Main")
-local Tab = Window:NewTab("Player")
-local Tab = Window:NewTab("Misc")
+local Window = Library.CreateLib("TITLE", "Midnight")
+
+local Tab = Window:NewTab("TabName")
+
+local Section = Tab:NewSection("Section Name")
+
+Section:NewLabel("LabelText")
+
+Section:NewButton("ButtonText", "ButtonInfo", function()
+    print("Clicked")
+end)
