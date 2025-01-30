56d3df30c0b1d699a5f652eb68fbbf5710691a97
vvywasejected <vvy6816@gmail.com>
2025-01-30 22:59:22 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,2 +1,4 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Test GUI", "DarkTheme")
+
+print("WANO LAWWWWWWWWWWWWWWWWWWW")
