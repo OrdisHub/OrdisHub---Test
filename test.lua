c77f2f8ee24b8f3db138b6d6672d01572738e9e0
vvywasejected <vvy6816@gmail.com>
2025-01-31 00:30:24 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -34,36 +34,13 @@ local Window = Rayfield:CreateWindow({
     }
  })
 
-  -- Main Tab
-  local MainTab = Window:CreateTab("OrdisHubFrfr", 4483362458) -- Title, Image
-  local Section = Tab:CreateSection("Main Stuff")
-  
-  local Button = Tab:CreateButton({
-    Name = "ServerHop",
+ local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+ local Section = Tab:CreateSection("Section Example")
+
+ local Button = Tab:CreateButton({
+    Name = "Button Example",
     Callback = function()
-        local Player = game.Players.LocalPlayer    
-        local Http = game:GetService("HttpService")
-        local TPS = game:GetService("TeleportService")
-        local Api = "https://games.roblox.com/v1/games/"
-        
-        local _place,_id = game.PlaceId, game.JobId
-        -- Asc for lowest player count, Desc for highest player count
-        local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=10"
-        function ListServers(cursor)
-           local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-           return Http:JSONDecode(Raw)
-        end
-        
-        time_to_wait = 120 --seconds
-        
-        -- choose a random server and join every 2 minutes
-        while wait(time_to_wait) do
-           --freeze player before teleporting to prevent synapse crash?
-           Player.Character.HumanoidRootPart.Anchored = true
-           local Servers = ListServers()
-           local Server = Servers.data[math.random(1,#Servers.data)]
-           TPS:TeleportToPlaceInstance(_place, Server.id, Player)
-        end
+    -- The function that takes place when the button is pressed
     end,
  })
 
@@ -72,4 +49,15 @@ local Window = Rayfield:CreateWindow({
 
 
 
-  local PlayerTab = Window:CreateTab("Player", 4483362458) -- Title, Image
+
+
+
+
+
+
+
+
+
+
+
+Rayfield:LoadConfiguration()
