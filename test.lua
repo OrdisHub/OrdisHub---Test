784f7ac2b8fb2cac914d0d6373f8f9ff4f91553a
vvywasejected <vvy6816@gmail.com>
2025-02-01 01:50:06 +0000

    town+worldBosslocationTpsTest

--- test.lua
+++ test.lua
@@ -1,3 +1,4 @@
+
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
@@ -37,14 +38,18 @@ local Window = Rayfield:CreateWindow({
 
 
 
- -- Main Tab ------------------------------------------------------------------------------------------------------------------------------
+ ------------------------------------------------------------------------------------ Main Tab ------------------------------------------------------------------------------------
  local Tab = Window:CreateTab("Main") -- Title, Image
 
 
 
 
 
- -- Player Tab ------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+ ------------------------------------------------------------------------------------ AutoFarm Tab ------------------------------------------------------------------------------------
  local Tab = Window:CreateTab("AutoFarm") -- Title, Image
 
 
@@ -53,7 +58,7 @@ local Window = Rayfield:CreateWindow({
 
 
 
-  -- Auto Farm Tab ------------------------------------------------------------------------------------------------------------------------------
+ ------------------------------------------------------------------------------------ Player Tab ------------------------------------------------------------------------------------  
   local Tab = Window:CreateTab("Player") -- Title, Image
 
 
@@ -61,13 +66,111 @@ local Window = Rayfield:CreateWindow({
 
 
 
+  ----------------------------------------------------------------------------------- Teleports Tab ------------------------------------------------------------------------------------
+  local Tab = Window:CreateTab("Teleport") -- Title, Image
+
+
+ -- Town Teleports ------------------------------------------------------------------------------------
+  local Section = Tab:CreateSection("Town Teleports")
+
+  local Button = Tab:CreateButton({
+   Name = "Shijo Town | Level 1",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+          player.Character.HumanoidRootPart.CFrame = CFrame.new(4317.55469, 653.036743, -1139.77734)
+      end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Umi Village | Level 60+",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+          player.Character.HumanoidRootPart.CFrame = CFrame.new(2775.34619, 669.725342, 270.112671)
+      end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Numa Temple | Level 120+",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+          player.Character.HumanoidRootPart.CFrame = CFrame.new(6074.58105, 982.393555, -476.648346)
+      end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Kura Camp | Level 180+",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+          player.Character.HumanoidRootPart.CFrame = CFrame.new(8515.73926, 773.933716, 1577.75525)
+      end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Yuki Fortress | Level 240+",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+          player.Character.HumanoidRootPart.CFrame = CFrame.new(3779.62646, 1440.41016, 2265.01685)
+      end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Yuki Fortress | Level 240+",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+          player.Character.HumanoidRootPart.CFrame = CFrame.new(3779.62646, 1440.41016, 2265.01685)
+      end
+   end,
+})
+
+
+-- World Boss TPs  ------------------------------------------------------------------------------------
+local Section = Tab:CreateSection("World Boss Spawn Locations")
+
+local Button = Tab:CreateButton({
+   Name = "Mantis Boss",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+          player.Character.HumanoidRootPart.CFrame = CFrame.new(5248.64893, 727.586365, -2174.27515)
+      end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Monk Boss",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+          player.Character.HumanoidRootPart.CFrame = CFrame.new(4064.35645, 634.277832, 77.6425781)
+      end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "EggJaw",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+          player.Character.HumanoidRootPart.CFrame = CFrame.new(6525.77441, 697.738342, 3276.10059)
+      end
+   end,
+})
 
 
 
 
-
-
-   -- Misc Tab ------------------------------------------------------------------------------------------------------------------------------
+ ------------------------------------------------------------------------------------ Misc Tab ------------------------------------------------------------------------------------
  local Tab = Window:CreateTab("Misc") -- Title, Image
  local Section = Tab:CreateSection("Server Commands")
 
